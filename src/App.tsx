604bd2de544b050bf82e5b5320c251b68d766b5a
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2025-07-09 01:35:52 +0000

    Style main card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { MainCard } from "./component/MainCard";
 import { ToggleSwitch } from "./component/ToggleSwitch";
+import facebook from "/assets/images/icon-facebook.svg";
+import twitter from "/assets/images/icon-twitter.svg";
+import instagram from "/assets/images/icon-instagram.svg";
+import youtube from "/assets/images/icon-youtube.svg";
+
 import "/src/App.css";
 
 const App = () => {
@@ -13,6 +19,44 @@ const App = () => {
           <p className="title">Dark Mode</p>
           <ToggleSwitch />
         </div>
+        <aside className="card_container">
+          <MainCard
+            platform={facebook}
+            username="@nathanf"
+            followers="1987"
+            change={12}
+            changeColor="var(--White-Text)"
+            borderTopColor="hsl(208, 92%, 53%)"
+            borderTopGradient=""
+          />
+          <MainCard
+            platform={twitter}
+            username="@nathanf"
+            followers="1044"
+            change={99}
+            changeColor="var(--Lime-Green)"
+            borderTopColor="hsl(203, 89%, 53%)"
+            borderTopGradient=""
+          />
+          <MainCard
+            platform={instagram}
+            username="@realnathanf"
+            followers="11k"
+            change={1099}
+            changeColor="var(--Lime-Green)"
+            borderTopGradient="linear-gradient(to right, hsl(37, 97%, 70%),hsl(329, 70%, 58%))"
+            borderTopColor=""
+          />
+          <MainCard
+            platform={youtube}
+            username="Nathan F."
+            followers="8239"
+            change={144}
+            changeColor="var(--YouTube)"
+            borderTopColor="hsl(348, 97%, 39%)"
+            borderTopGradient=""
+          />
+        </aside>
       </section>
     </main>
   );
---
+++ src/component/MainCard.tsx
@@ -0,0 +1,58 @@
+import "/src/component/MainCard.css";
+
+interface MainCardProps {
+  platform: string;
+  username: string;
+  followers: string;
+  change: number;
+  changeColor: string;
+  borderTopColor: string;
+  borderTopGradient: string;
+}
+
+export const MainCard: React.FC<MainCardProps> = ({
+  platform,
+  username,
+  followers,
+  change,
+  changeColor,
+  borderTopColor,
+  borderTopGradient,
+}) => {
+  return (
+    <main
+      className="main_card"
+      style={
+        borderTopGradient
+          ? {} // Don't use border-top if using gradient
+          : { borderTop: `4px solid ${borderTopColor}` }
+      }
+    >
+      {borderTopGradient && (
+        <div
+          className="card_border"
+          style={{ background: borderTopGradient }}
+        ></div>
+      )}
+      <div className="user_details">
+        <img src={platform} alt="facebook-icon" className="facebook" />
+        <p className="user_name">{username}</p>
+      </div>
+      <div className="followers">
+        <p className="social_followers">{followers}</p>
+        <p className="followers_title">Followers</p>
+      </div>
+      <div className="current_day">
+        <div className="option_shape"></div>
+        <p
+          className="current_followers"
+          style={{
+            color: `${changeColor}`,
+          }}
+        >
+          {change} Today
+        </p>
+      </div>
+    </main>
+  );
+};
